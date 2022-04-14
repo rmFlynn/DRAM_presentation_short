4a8d9cfebaaa7f166885a9a09e8ca1445107219d
rmflynn <rmfofco@gmail.com>
2022-04-14 11:04:12 -0600

    Update empty cells figure and genome

--- media/live/make_html_data.py
+++ media/live/make_html_data.py
@@ -1,17 +1,16 @@
+"""Make tables"""
 import pandas as pd
 from pretty_html_table import build_table
 
 df = pd.read_csv("annotations.tsv", sep='\t', index_col=0)
 df = df.head(200)
-html_table_blue_light = build_table(df, 'blue_light')
-print(html_table_blue_light)
+html_table = build_table(df, 'grey_light')
 with open('annotations.html', 'w') as f:
-    f.write(html_table_blue_light)
+    f.write(html_table)
 
 df = pd.read_csv("./genome_stats.tsv", sep='\t', index_col=0)
 df = df.head(200)
-html_table_blue_light = build_table(df, 'blue_light')
-print(html_table_blue_light)
+html_table = build_table(df, 'gray_light')
 with open('genome_stats.html', 'w') as f:
-    f.write(html_table_blue_light)
+    f.write(html_table)
 
